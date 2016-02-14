d134b5c93db417f431c4722339368c43d689c324
Neeraj Lad <neeraj.lad@yandex.com>
2016-02-14 14:47:42 +0530

    Complete API access

--- front-end-development/intermediate/weather-app/js/main.js
+++ front-end-development/intermediate/weather-app/js/main.js
@@ -1,20 +1,25 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  
+  var openWeatherURL = "";
+  
   
   if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(position) {
 
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
-    var openWeatherURL = "api.openweathermap.org/data/2.5/weather?lat=" + Math.round(latitude) + "&lon=" + Math.round(longitude) + "&appid=44db6a862fba0b067b1930da0d769e98";
+    openWeatherURL += "http://api.openweathermap.org/data/2.5/weather?lat=" + Math.round(latitude) + "&lon=" + Math.round(longitude) + "&appid=44db6a862fba0b067b1930da0d769e98";
     
-        $(".data").html(openWeatherURL);
+
     
-    $.getJSON(openWeatherURL, function(val){
-      alert(val);
-      var keys = Object.keys(val);
-      $(".data").html(keys);
-    });
      
-  });
+  }); //end navigator.geolocation if clause
 }
-});
+    $.getJSON(openWeatherURL, function(val){
+          
+          var location = val["name"];
+          $(".location").html("");
+          $(".location").html("<h3>" + openWeatherURL + "</h3>");          
+    
+    }); //end getJSON
+}); //end document ready
